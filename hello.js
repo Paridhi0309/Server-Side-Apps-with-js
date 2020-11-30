5989e52d0cdb25434a3e90a91603e640a1ba235a
Paridhi <4paridhi@gmail.com>
2020-11-30 20:01:49 +0530

    node

---
+++ hello.js
@@ -0,0 +1,30 @@
+/*Response from server to the client*/
+
+var http=require('http');
+const moment = require('moment');
+function serverCallback(req,res){
+var begin_time= moment("10:00","HH:mm");
+var end_time= moment("18:00","HH:mm");
+
+var message="Hello "+process.argv[2]+"\n";
+message+="Welcome to our page.\n";
+message+="Now,it is"+ moment().format("HH:mm")+ "\n";
+message+="Our business hour is from "+begin_time.format("HH:mm")+"to "+end_time;
+
+var begin_difference=begin_time.diff( moment(),'minutes');
+var end_difference=moment().diff(end_time,'minutes');
+
+if(begin_difference>0)
+{
+    message+="Please come back in "+begin_difference+"minutes \n";
+
+}
+if(end_difference>0)
+{
+    message+="Please come back tomorrow."+"\n";
+}
+
+res.writeHead(200,{'Content-Type':'text/plain'});
+res.end("Hello "+process.argv[2]+".It is "+moment().format('LLLL')+"now.");
+}
+http.createServer(serverCallback).listen(8080);
